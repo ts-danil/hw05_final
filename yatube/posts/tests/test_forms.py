37dba5aa6cc10a2eeb562d774a3415341ec430a5
Данил Цопанов <danil1198@yandex.ru>
2022-08-19 18:40:09 +0300

    isort edit

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
-
 from posts.models import Group, Post
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
