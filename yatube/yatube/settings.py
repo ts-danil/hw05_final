16051f565d9c045e8a23be114b7bac67864c34eb
Данил Цопанов <danil1198@yandex.ru>
2023-01-20 11:49:50 +0300

    Media path edit

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = './media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = [
